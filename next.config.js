436fc79d73160fc72561120692240e7f8521155f
Matt <32686182+matthails@users.noreply.github.com>
2022-07-19 19:31:03 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -3,5 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
-
+ 
 module.exports = nextConfig
+ 
+const withMDX = require('@next/mdx')({
+  extension: /\.(md|mdx)$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+})
+
